56050d3f308ad33f5fb8730278aeda126a41df61
rojajullapalli <rojajules@gmail.com>
2021-08-24 18:14:10 +0530

    ADDED:Created Simple Linked List

---
+++ src/com/bridgelabz/list/LinkedList.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.list;
+
+public class LinkedList {
+    Node node;
+    Node tail;
+    Node head;
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            this.node = newNode;
+            this.tail = newNode;
+            this.head = newNode;
+        }else{
+            Node temp=head;
+            head=newNode;
+            head.next=temp;
+        }
+    }
+
+    public void display(){
+        Node current = head;
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        while(current != null) {
+
+            System.out.print(current.data + " "  );
+            current = current.next;
+        }
+
+    }
+
+}
--- src/com/bridgelabz/list/LinkedListMain.java
+++ src/com/bridgelabz/list/LinkedListMain.java
@@ -3,5 +3,11 @@ package com.bridgelabz.list;
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("welcome to linkedlist program");
+        LinkedList list = new LinkedList();
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        System.out.println("Nodes of singly linked list:");
+        list.display();
     }
 }
