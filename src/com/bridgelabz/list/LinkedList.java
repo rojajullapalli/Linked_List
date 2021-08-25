f2bb66d18049636dfd2bc469062a3ca35aa6c5de
rojajullapalli <rojajules@gmail.com>
2021-08-25 12:08:22 +0530

    ADDED:Master Changes

--- src/com/bridgelabz/list/LinkedList.java
+++ src/com/bridgelabz/list/LinkedList.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.list;
 
-public class LinkedList {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LinkedList<T extends Comparable <T>> {
+    ArrayList<Integer> li = new ArrayList<Integer>();
     Node node;
     Node tail;
     Node head;
@@ -12,9 +16,8 @@ public class LinkedList {
             this.tail = newNode;
             this.head = newNode;
         }else{
-            Node temp=head;
-            head=newNode;
-            head.next=temp;
+           tail.next = newNode;
+           tail = newNode;
         }
     }
 
@@ -38,6 +41,39 @@ public class LinkedList {
         head = head.next;
     }
 
+    public void popLast() {
+        if (head == null)
+            System.out.println("List is Empty");
+        Node last = head;
+        Node temp = null;
+        while (last.next != null){
+            temp = last;
+            last = last.next;
+        }
+        temp.next = null;
+        last.next = temp.next;
+    }
+
+    public void search(int key){
+        Node current = head;
+        int count = 1;
+        int found = 0;
+        while(current != null){
+            if (current.data == key){
+                found=1;
+                break;
+            }
+            count = count + 1;
+            current = current.next;
+
+        }
+        if (found ==1 )
+            System.out.println("\nNode with value "+key+" is found at position " +count);
+        else
+            System.out.println("Element is not present in the list");
+    }
+
+
     public void display(){
         Node current = head;
         if(head == null) {
@@ -52,4 +88,52 @@ public class LinkedList {
 
     }
 
+    public void remove(int key) {
+        Node temp = head, prev = null;
+
+        // If head node itself holds the key to be deleted
+        if (temp != null && temp.data == key) {
+            head = temp.next; // Changed head
+            return;
+        }
+
+        // Search for the key to be deleted, keep track of
+        // the previous node as we need to change temp.next
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // If key was not present in linked list
+        if (temp == null)
+            return;
+
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+    public void size(){
+        Node temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        System.out.println("\nsize of the linkedlist is "+count);
+
+    }
+    public void addToArrayList(){
+        Node temp=head;
+        int i=0;
+        while(temp!=null){
+            li.add(i,temp.data);
+            i++;
+            temp=temp.next;
+        }
+        getsort(li);
+
+    }
+    public void getsort(ArrayList<Integer> list){
+        Collections.sort(list);
+        System.out.println("the elements stored in arraylist"+list);
+    }
 }
--- src/com/bridgelabz/list/LinkedListMain.java
+++ src/com/bridgelabz/list/LinkedListMain.java
@@ -4,12 +4,16 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("welcome to linkedlist program");
         LinkedList list = new LinkedList();
-        list.append(70);
         list.append(56);
+        list.append(70);
         list.insertAfter(list.head, 30);
         System.out.println("Nodes of singly linked list:");
         list.display();
-        list.pop();
+        list.search(30);
+        list.insertAfter(list.head.next,40);
+        System.out.println("Nodes after addded 40 to list");
         list.display();
+        list.size();
+        list.addToArrayList();
     }
 }
