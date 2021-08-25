90661a06d09c4453716acaf3d63db26a449b8758
rojajullapalli <rojajules@gmail.com>
2021-08-25 12:17:33 +0530

    MODIFIED:Append-Method

--- src/com/bridgelabz/list/LinkedListMain.java
+++ src/com/bridgelabz/list/LinkedListMain.java
@@ -4,9 +4,9 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("welcome to linkedlist program");
         LinkedList list = new LinkedList();
-        list.append(70);
-        list.append(30);
         list.append(56);
+        list.append(30);
+        list.append(70);
         System.out.println("Nodes of singly linked list:");
         list.display();
     }
